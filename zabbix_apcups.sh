da85e201f87690f23023a8c4a5d5f7efc75e5741
GI_Jack <GI_Jack@hackermail.com>
2021-06-23 17:06:30 -0400

    updated check script with more checks

--- zabbix_apcups.sh
+++ zabbix_apcups.sh
@@ -20,13 +20,31 @@ DATA="$(${APCACCESS} -h ${UPS_HOST}:${UPS_PORT} )"
 # Check online status. convert this to binary data
 status=$(echo "${DATA}" |grep -m1 STATUS | cut -d ":" -f 2)
 if [ ${status} == "ONLINE" ];then
-  status=1
+  is_online=1
  else
-  status=0
+  is_online=0
 fi
 
-zsend ups.batterypercent $(echo "${DATA}" |grep -m1 BCHARGE | cut -d ":" -f 2)
-zsend ups.timeleft $(echo "${DATA}" |grep -m1 TIMELEFT | cut -d ":" -f 2)
-zsend ups.load $(echo "${DATA}" |grep -m1 LOADPCT | cut -d ":" -f 2)
-zsend ups.voltage $(echo "${DATA}" |grep -m1 LINEV | cut -d ":" -f 2)
-zsend ups.status $(echo ${status} )
+INFO=$(cat << EOF
+$(echo "${DATA}" |grep -m1 DATE )
+$(echo "${DATA}" |grep -m1 VERSION )
+$(echo "${DATA}" |grep -m1 DRIVER )
+$(echo "${DATA}" |grep -m1 MODEL )
+$(echo "${DATA}" |grep -m1 UPSNAME )
+$(echo "${DATA}" |grep -m1 SERIALNO )
+$(echo "${DATA}" |grep -m1 BATTDATE )
+$(echo "${DATA}" |grep -m1 NOMPOWER )
+$(echo "${DATA}" |grep -m1 FIRMWARE )
+EOF
+)
+
+
+# update daemon
+zsend ups.batterypercent $(echo "${DATA}" |grep -m1 BCHARGE | cut -d ":" -f 2| cut -d " " -f 2)
+zsend ups.timeleft $(echo "${DATA}" |grep -m1 TIMELEFT | cut -d ":" -f 2| cut -d " " -f 2)
+zsend ups.load $(echo "${DATA}" |grep -m1 LOADPCT | cut -d ":" -f 2|cut -d " " -f 2)
+zsend ups.voltage $(echo "${DATA}" |grep -m1 LINEV | cut -d ":" -f 2|cut -d " " -f 2)
+zsend ups.bvoltage $(echo "${DATA}" |grep -m1 BATTV | cut -d ":" -f 2|cut -d " " -f 2)
+zsend ups.status ${status}
+zsend ups.is_online ${is_online}
+zsend ups.display_info "${INFO}"
