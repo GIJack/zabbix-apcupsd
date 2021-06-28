921eb125fc29e5e0aa78f6103bcd44cdc3bc3387
GI_Jack <GI_Jack@hackermail.com>
2021-06-28 12:18:43 -0400

    add text status if connect to battery is lost, COMMLOST, status

--- zabbix_apcups.sh
+++ zabbix_apcups.sh
@@ -14,15 +14,22 @@ zsend() {
   ${ZABBIX_SENDER} -c "${ZABBIX_CONF}" -k $1 -o "${2}"
 }
 
+datestamp() {
+  # date with same format used by apcaccess
+  local formatting="+%F %T %z"
+  date "${formatting}"
+}
+
 apcaccess_failed() {
   echo 1>&2  "apcaccess command failed. monitoring is broken, please check apcaccess and apcupsd to ensure it is setup and configured correctly. Also check config at top of this script."
-  DISPLAY_INFO="apcaccess command failed.
-please ensure that apcupsd is running correctly, and that the zabbix_apcups.sh
-script is setup correctly. Config is at the top.
+  INFO="DATE	: $(datestamp)
+apcaccess command failed. please ensure that apcupsd is running correctly, and
+that the zabbix_apcups.sh script is setup correctly. Config is at the top.
+try running apcaccess on the command line.
 "
   zsend ups.is_online 0
   zsend ups.status "¡software_failed!"
-  zsend ups.display_info "${DISPLAY_INFO}"
+  zsend ups.display_info "${INFO}"
   exit 1
 }
 
@@ -37,8 +44,14 @@ main() {
    else
     is_online=0
   fi
-
-  INFO=$(cat << EOF
+  
+  if [ ${status} == "COMMLOST" ];then
+  INFO="$(echo "${DATA}" |grep -m1 DATE )
+Communication from host to APC UPS device has been lost. Please ensure cables
+are connected and UPS remains operative and USB ports on host are operational.
+"
+  else
+    INFO=$(cat << EOF
 $(echo "${DATA}" |grep -m1 DATE )
 $(echo "${DATA}" |grep -m1 VERSION )
 $(echo "${DATA}" |grep -m1 DRIVER )
@@ -49,8 +62,9 @@ $(echo "${DATA}" |grep -m1 BATTDATE )
 $(echo "${DATA}" |grep -m1 NOMPOWER )
 $(echo "${DATA}" |grep -m1 FIRMWARE )
 EOF
-  )
-
+    )
+  fi
+  
   # update daemon
   zsend ups.batterypercent $(echo "${DATA}" |grep -m1 BCHARGE | cut -d ":" -f 2| cut -d " " -f 2)
   zsend ups.timeleft $(echo "${DATA}" |grep -m1 TIMELEFT | cut -d ":" -f 2| cut -d " " -f 2)
